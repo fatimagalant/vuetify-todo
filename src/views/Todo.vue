3ee425285f5345d6807663827938b6c2815d8aa5
unknown <fatimagalant03@gmail.com>
2022-09-27 16:21:26 +0200

    first commit

---
+++ src/views/Todo.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="home pa-6">
+ <v-text-field
+ v-model="newTaskTitle"
+ @click:append="addTask"
+ @keyup.enter="addTask"
+ class="pa-3"
+            outlined
+            label="Add Task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
+ <v-list class="pt-0"
+      flat
+    >
+  <div
+        v-for="task in tasks"
+        :key="task.id"
+  >
+        <v-list-item
+        @click="doneTask(task.id)"
+        :class="{ 'deep-purple lighten-4' : task.done }"
+        >
+
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{'text-decoration-line-through' : task.done}">
+              {{task.title}}</v-list-item-title>
+            </v-list-item-content>
+          <v-list-item-action>
+          <v-btn 
+          @click.stop="deleteTask(task.id)"
+          icon>
+            <v-icon color="red lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+          </template>
+
+        </v-list-item>
+          <v-divider></v-divider>
+</div>
+    </v-list>
+</div>
+
+
+</template>
+
+<script>
+
+  export default {
+    name: 'Todo',
+    data () {
+    return {
+    newTaskTitle: '',
+     tasks :[
+   
+    ]
+    }
+    },
+    methods: {
+    addTask() {
+    let newTask = {
+    id: Date.now(),
+    title: this.newTaskTitle,
+    done: false
+    }
+    this.tasks.push(newTask)
+    this.newTaskTitle = ''
+    },
+    doneTask(id) {
+    let task = this.tasks.filter(task => task.id === id) [0]
+    task.done = !task.done
+    },
+    deleteTask(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+    }
+  }
+</script>
+<style scoped>
+</style>
